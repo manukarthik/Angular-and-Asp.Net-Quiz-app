19e79beb560a0d0ffdda03b78282b4952a1550dd
manukarthik <manukarthikkr@gmail.com>
2018-07-25 00:28:02 -0400

    Added Jwt handler in the controller

---
+++ quiz-backend/quiz-backend/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace quiz_backend.Controllers
+{
+    public class Credentials
+    {
+        public string Email { get; set; }
+        public string password { get; set; }
+    }
+    [Produces("Application/json")]
+    [Route("api/Account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] Credentials credentials)
+        {
+            var jwt = new JwtSecurityToken();
+            return Ok(new JwtSecurityTokenHandler().WriteToken(jwt));
+        }
+    }
+}
